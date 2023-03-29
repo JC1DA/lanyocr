b895f5647478910314a9a0f7cb13bf3f4c19f31e
JC1DA <jc1da.3011@gmail.com>
2023-03-28 22:53:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "lanyocr.text_recognizer",
         "lanyocr.text_recognizer.dicts",
     ],  # Chose the same as "name"
-    version="0.1.7",  # Start with a small number and increase it with every change you make
+    version="0.1.8",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="An OCR library for Python",  # Give a short description about your library
     long_description=readme,
